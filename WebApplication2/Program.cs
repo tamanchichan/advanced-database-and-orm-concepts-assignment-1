bff5f61889e7b5a325efe4f866d47e916e51655b
tamanchichan <tamanchichan@gmail.com>
2023-08-03 23:47:06 -0500

    first endpoint

--- WebApplication2/Program.cs
+++ WebApplication2/Program.cs
@@ -1,4 +1,5 @@
 using LaptopStoreRefactorDb.Data;
+using LaptopStoreRefactorDb.Model;
 using Microsoft.AspNetCore.Http.Json;
 using Microsoft.EntityFrameworkCore;
 using System.Text.Json.Serialization;
@@ -26,11 +27,58 @@ using (IServiceScope scope = app.Services.CreateScope())
   await SeedData.Initialize(services);
 }
 
-app.MapGet("/laptops/search", (LaptopStoreRefactor db) =>
+app.MapGet("/laptops/search", (LaptopStoreRefactor db, decimal? amountAbove, decimal? amountBelow, Guid? storeNumber, string? province, LaptopCondition? condition, Guid? brandId, string? searchPhrase) =>
 {
   try
   {
-    return Results.Ok(db.Laptops.ToHashSet());
+    var laptopAndStore = db.LaptopsAndStores.AsQueryable();
+
+    if (amountAbove != null)
+    {
+      laptopAndStore = laptopAndStore.Where(ls => ls.Laptop.Price > amountAbove);
+    }
+
+    if (amountBelow != null)
+    {
+      laptopAndStore = laptopAndStore.Where(ls => ls.Laptop.Price < amountBelow);
+    }
+
+    if (storeNumber != null)
+    {
+      laptopAndStore = laptopAndStore.Where(ls => ls.Store.Id == storeNumber && ls.Quantity > 0);
+    }
+    else if (!String.IsNullOrEmpty(province))
+    {
+      laptopAndStore = laptopAndStore.Where(ls => ls.Store.Province == province && ls.Quantity > 0);
+    }
+
+    if (condition != null)
+    {
+      laptopAndStore = laptopAndStore.Where(ls => ls.Laptop.Condition == condition);
+    }
+
+    if (brandId != null)
+    {
+      laptopAndStore = laptopAndStore.Where(ls => ls.Laptop.BrandId == brandId);
+    }
+
+    if (searchPhrase != null)
+    {
+      laptopAndStore = laptopAndStore.Where(ls => ls.Laptop.Model.Contains(searchPhrase));
+    }
+
+    return Results.Ok(laptopAndStore.Select(ls => new
+    {
+      Laptop = new
+      {
+        Id = ls.Laptop.Id,
+        Model = ls.Laptop.Model,
+        Brand = ls.Laptop.Brand,
+        Price = ls.Laptop.Price,
+        Condition = ls.Laptop.Condition,
+        Quantity = ls.Quantity
+      }
+    }));
   }
   catch (Exception ex)
   {
